0df8ef8a4561dcb6999c5beacf14d51bc7fb30d6
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-18 20:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function threeSum(arr, target) {
 		let p = i+1; 
 		let q = arr.length - 1;
 		while(p<q){
-			let sum = p + q + i;
+			let sum = arr[p] + arr[q] + arr[i];
 			if(Math.abs(target-sum) < min_diff){
 				min_diff == target - sum;
 				ans = sum;
