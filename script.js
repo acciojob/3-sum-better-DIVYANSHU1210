cdd123a01bc5064c7c90c1d61bc96644d153798c
DIVYANSHU1210 <84267089+DIVYANSHU1210@users.noreply.github.com>
2023-09-18 20:40:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function threeSum(arr, target) {
 // write your code here
 	arr.sort((a,b)=>a-b);
 	let min_diff = Number.MAX_VALUE;
+	let ans = 0;
 	for(let i=0; i<arr.length; i++){
 		let p = i+1; 
 		let q = arr.length - 1;
@@ -9,6 +10,7 @@ function threeSum(arr, target) {
 			let sum = p + q + i;
 			if(Math.abs(target-sum) < min_diff){
 				min_diff == target - sum;
+				ans = sum;
 			}
 
 			if(target < sum){
@@ -18,7 +20,7 @@ function threeSum(arr, target) {
 		}	
 	}
 
-	return min_diff;
+	return ans;
 	
 }
 
